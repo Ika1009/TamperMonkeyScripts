93a2009fc0e5e0054422867a4839f0a207c5da1d
Ika1009 <ilija.doncic@yahoo.com>
2023-03-05 19:03:27 +0100

    Biranje datuma

--- FacebookSearchPageData.js
+++ FacebookSearchPageData.js
@@ -1,68 +1,84 @@
 // ==UserScript==
 // @name         Facebook Search Data Automation
 // @namespace    http://tampermonkey.net/
-// @version      2.2
+// @version      2.51
 // @description  Extract Data that match the criteria from Facebook Search Page
 // @author       Doncha1009
 // @match        https://www.facebook.com/search/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
+ 
     window.addEventListener("load", async function()
     {
         const delay = ms => new Promise(res => setTimeout(res, ms));
-
-        console.log("starting in 20 seconds...");
-
-        await delay(20000);
-
-        const datum = new Date("2023-1-26");
+ 
+        console.log("starting in 120 seconds...");
+ 
+        await delay(12000/*0*/);
+ 
+        /*const datum = new Date("2023-1-31");
         const sadasnji = new Date();
         if(sadasnji > datum){
-            console.log("The trial expired");
+            console.log("The trial expired, if you could pay on this Paypal and not on Fiverr because Fiverr takes a big share: https://www.paypal.com/paypalme/Donca1009");
             return;
-        }
-
+        }*/
+ 
         console.log("starting...");
+        let DateFirstBoundary = promptForDate("Enter a first date boundary (e.g. June 8, 2010):")
+        let DateSecondBoundary = promptForDate("Enter a second date boundary (e.g. June 8, 2010):")
+
         let sviRezultati = "Buisness Name,Phone Number,Address,Website,Facebook,Creation Date, Category, Email\n";
         let lista_ljudi = document.getElementsByClassName("x193iq5w x1xwk8fm")[0].children;
         let lista = [];
-
         //for petlja da prodje kroz sve klase da nadje samo url biznis strana
-        for(let i = 0; i < lista_ljudi.length - 1; i++)
+        /*for(let i = 0; i < lista_ljudi.length - 1; i++)
         {
             lista[i] = lista_ljudi[i].children[0].children[0].children[0].children[0].children[0].children[0].children[1].children[0].children[0].children[0].children[0].children[0].children[0].children[0].getAttribute("href");
-        }
-
-        /*lista[0] = "";
-        lista[1] = "";
-        lista[2] = "";
-        lista[3] = "";
-        lista[4] = "";*/
-
+        }*/
+ 
+        lista[0] = "https://www.facebook.com/profile.php?id=100082873075641";
+        lista[1] = "https://www.facebook.com/profile.php?id=100083414017433";
+        lista[2] = "https://www.facebook.com/profile.php?id=100084155977753";
+        lista[3] = "https://www.facebook.com/profile.php?id=100085804250570";
+        lista[4] = "https://www.facebook.com/profile.php?id=100088170301246";
+        lista[5] = "https://www.facebook.com/profile.php?id=100086963957020";
+        lista[6] = "https://www.facebook.com/profile.php?id=100087732184149";
+        lista[7] = "https://www.facebook.com/profile.php?id=100087661926233";
+        lista[8] = "https://www.facebook.com/profile.php?id=100087007830841";
+        lista[9] = "https://www.facebook.com/profile.php?id=100082819498005";
+        lista[10] = "https://www.facebook.com/BensMowingAndHandyman";
+        lista[11] = "https://www.facebook.com/profile.php?id=100089776776088";
+        lista[12] = "https://www.facebook.com/profile.php?id=100083271615586";
+        lista[13] = "https://www.facebook.com/profile.php?id=100085593604065";
+        lista[14] = "https://www.facebook.com/profile.php?id=100085593604065";
+        lista[15] = "https://www.facebook.com/profile.php?id=100082588638460";
+        lista[16] = "";
+ 
+ 
+ 
         //await delay(5000);
         console.log("The number of buisness pages: " + lista.length);
         let pauzaBr = 1;
         for(let i = 0; i < lista.length - 1; i++)
         {
-            if(i == pauzaBr * 250) { pauzaBr++; await delay(1800000); } // ceka pola sata da facebook ne bi blokirao
+            //if(i == pauzaBr * 250) { pauzaBr++; await delay(1800000); } // ceka pola sata da facebook ne bi blokirao
             let url = lista[i];
-            console.log(url)
-
+            //console.log(url)
+ 
             let datum, broj_telefona, adresa;
             console.log("starting " + i + " company");
-
+ 
             await delay(1000);
             let windowNovi = window.open(url); // za svaku biznis stranu otvori window
             await delay(5000);
             this.self.focus();
-
+ 
             let pageSource = windowNovi.document.getElementsByTagName("html")[0].innerHTML;
-
+ 
             if(pageSource.contains('page_creation_date":{"text":'))
                 datum = getPageCreationDate(pageSource);
             else
@@ -74,19 +90,19 @@
                 stringTemp = stringTemp.slice(0, stringTemp.indexOf('","'));
                 stringTemp += "_profile_transparency"; // da bi uslo odmah za datum
                 stringTemp = stringTemp.replaceAll("\\/", "/"); // zamenjuje \/ sa /
-
+ 
                 await delay(1000);
                 windowNovi.close();
                 let windowNoviji = window.open(stringTemp);
                 await delay(5000);
                 this.self.focus();
-
+ 
                 let pageSource2 = windowNoviji.document.getElementsByTagName("html")[0].innerHTML;
                 //console.log(pageSource2);
-
+ 
                 await delay(1000);
                 windowNoviji.close();
-
+ 
                 datum = getPageCreationDate2(pageSource2);
             }
             //console.log("Datum: " + datum);
@@ -94,7 +110,7 @@
             //console.log("Broj Telefona: " + broj_telefona);
             adresa = getCountryName(pageSource);
             //console.log("Ime zemlje:" + adresa);
-
+ 
             if(datum == -1 || broj_telefona == -1 || adresa == -1){ // da li su ispravni
                 //console.log("Nije dobar jer je datum: " + datum + ", broj telefona: " + broj_telefona + ", adresa: " + adresa);
             }
@@ -102,7 +118,7 @@
             {
                 if(adresa.indexOf("&gt") != -1)
                     adresa = adresa.slice(adresa.indexOf("&gt") + 8, adresa.length - 1);
-
+ 
                 let imeBiznisa = getBuisnessName(pageSource);
                 let kategorija = getBuisnessCategory(pageSource);
                 //console.log("Ime zemlje Biznisa:" + imeBiznisa);
@@ -111,21 +127,25 @@
                     website = "There is no website";
                 else
                     website = website.replaceAll("\\/", "/");
-
+ 
                 let email = getBuisnessEmail(pageSource);
                 console.log("company matches the criteria: " + imeBiznisa + "  ");
-
+ 
                 sviRezultati += '"' + imeBiznisa + '",' + broj_telefona + "," + '"' + adresa + '","' + website + '","' +  url + '","' + datum + '","' + kategorija + '","' + email + '",' + "\n";
             }
             await delay(1000);
             windowNovi.close();
             await delay(1000);
-
+ 
         }
-
+ 
         saveData(sviRezultati, "foundData.csv");
-
-
+ 
+        function promptForDate(promptString) {
+            let dateString = prompt(promptString);
+            let dateObject = new Date(Date.parse(dateString));
+            return dateObject;
+        }
         function getPageCreationDate(pageSource)
         {
             let leviDeoIndex = pageSource.indexOf('page_creation_date":{"text":');
@@ -133,28 +153,33 @@
             //console.log(stringDatuma);
             if(stringDatuma.contains("2022")){
                 stringDatuma = stringDatuma.slice(stringDatuma.indexOf(' - ') + 3, stringDatuma.indexOf('"},'));
-
-                if(stringDatuma.contains(`\\u`))
-                    return stringDatuma.split("\\u")[1];
-                return stringDatuma;
+                let datum = new Date(Date.parse(stringDatuma));
+                if(isBetweenDates(datum))
+                {
+                    if(stringDatuma.contains(`\\u`))
+                        return stringDatuma.split("\\u")[1];
+                    return stringDatuma;
+                }
+                else
+                    return -1;
             }
-            else
-                return -1;
+            return -1;
         }
         function getPageCreationDate2(pageSource)
         {
             let leviDeoIndex = pageSource.indexOf('"},"field_type":"creation_date"');
             let stringDatuma = pageSource.substring(leviDeoIndex - 20, leviDeoIndex + 1);
-            if(!stringDatuma.contains("2022"))
-                return -1;
-            //console.log("DATUM 2: " + stringDatuma);
-            if(stringDatuma.contains("2021") || stringDatuma.contains("2020") || stringDatuma.contains("2019") || stringDatuma.contains("2018") || stringDatuma.contains("2007") ||
-            stringDatuma.contains("2017") || stringDatuma.contains("2016") || stringDatuma.contains("2015") || stringDatuma.contains("2014") || stringDatuma.contains("2009") ||
-            stringDatuma.contains("2013") || stringDatuma.contains("2012") || stringDatuma.contains("2011") || stringDatuma.contains("2010") || stringDatuma.contains("2008") ||
-            (stringDatuma.contains("January") && stringDatuma.contains("2022")))
-                return -1;
-            else
-                return stringDatuma.slice(stringDatuma.indexOf('":"') + 3, stringDatuma.length - 1);
+            stringDatuma = stringDatuma.slice(stringDatuma.indexOf('":"') + 3, stringDatuma.length - 1);
+            let datum = new Date(Date.parse(stringDatuma));
+            if(isBetweenDates(datum))
+                return stringDatuma;
+            return -1;
+        }
+        function isBetweenDates(dateObjectPage) {
+            let timeCurrentPage = dateObjectPage.getTime();
+            let timeFirst = DateFirstBoundary.getTime();
+            let timeSecond = DateSecondBoundary.getTime();
+            return (timeCurrentPage > timeFirst && timeCurrentPage < timeSecond);
         }
         function getPhoneNumber(pageSource)
         {
@@ -166,7 +191,7 @@
                 stringBrojaTelefona = pageSource.substring(leviDeoIndex + 11, leviDeoIndex + 46);
                 if(leviDeoIndex == -1 || stringBrojaTelefona.contains("null"))
                     return -1;
-
+ 
                 return stringBrojaTelefona.slice(0, stringBrojaTelefona.indexOf('</'))
             }
             else
@@ -261,5 +286,5 @@
             window.URL.revokeObjectURL(url);
         };
     });
-
+ 
 })();
