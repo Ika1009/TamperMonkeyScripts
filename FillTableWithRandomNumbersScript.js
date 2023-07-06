6d96b16507bcf155cb7ed5a33099cce3e0c5c8bf
Ika1009 <ilija.doncic@yahoo.com>
2023-07-06 17:46:46 +0200

    popravljeno da ostane editovano i @match

--- FillTableWithRandomNumbersScript.js
+++ FillTableWithRandomNumbersScript.js
@@ -1,26 +1,27 @@
 // ==UserScript==
 // @name         Table Filler
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.2
 // @description  Takes the website and fills the last column for every row with random numbers
 // @author       Doncha1009
-// @match        *://*
+// @license      MIT
+// @match        https://www.firmtr.com/timecards2?id=*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
+    console.log("Started");
+ 
     // Get references to the elements
     const tableBody = document.getElementById('theTableBody');
     const moreRowsBtn = document.getElementById('moreRowsBtn');
-
-    console.log("Started");
+ 
     const minNumber = 0.02;
     const maxNumber = 0.1;
     let lastRowIndex = 0;
-
+ 
     // Loop through each row in the table body and fill it with a random number
     function fillTableRows() {
         if(lastRowIndex == tableBody.children.length) return;
@@ -29,30 +30,30 @@
             const lastCell = row.lastElementChild;
             const randomNumber = getRandomNumber(minNumber, maxNumber);
             // Set the random number as the content of the last cell
-            lastCell.textContent = randomNumber;
+            lastCell.children[0].value = randomNumber;
         }
         lastRowIndex = tableBody.children.length;
         console.log("filled");
     }
-
+ 
     fillTableRows();
-
+ 
     // Attach an event listener to the "+ more rows" button
     moreRowsBtn.addEventListener('click', function() {
         fillTableRows();
     });
-
+ 
     // Function to generate a random number between min and max (inclusive)
     function getRandomNumber(min, max) {
         const randomDecimal = Math.random();
         const scaledRandom = randomDecimal * (max - min);
         const randomNumber = scaledRandom + min;
-
+ 
         // Round the random number to two decimal places
         const roundedNumber = randomNumber.toFixed(2);
         // Return the rounded random number
         return parseFloat(roundedNumber);
     }
-
-
+ 
+ 
 })();
