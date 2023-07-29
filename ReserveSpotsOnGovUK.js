8337b298cf80f1e83199cf324411720b92eac1f8
Ika1009 <ilija.doncic@yahoo.com>
2023-07-29 16:32:10 +0200

    Napravljen kostur za nov u skriptu

---
+++ ReserveSpotsOnGovUK.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Reserve Free Spots
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Reserve the spots on the UK gov website and not get detected by the website
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
