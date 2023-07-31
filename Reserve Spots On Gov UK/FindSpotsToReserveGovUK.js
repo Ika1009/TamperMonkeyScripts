51066576ea5b4f31517b6afd381561f7f7c5e262
Ika1009 <ilija.doncic@yahoo.com>
2023-07-31 22:01:00 +0200

    dodavanje za proveravanje datuma

--- Reserve Spots On Gov UK/FindSpotsToReserveGovUK.js
+++ Reserve Spots On Gov UK/FindSpotsToReserveGovUK.js
@@ -40,9 +40,28 @@
                     }
                 }
             });
-            await NextWeek();
+
+            // Checking if two months from now and need to go back
+            let dateString = document.getElementsByClassName("centre bold")[0].innerText
+            if(isMoreThanTwoMonthsFromToday(dateString))
+                await VratiSeUnazad();
+            else
+                await NextWeek();
         } 
         
+        async function VratiSeUnazad()
+        {
+            await delay(1000);
+            const backWeekLink = document.getElementById("searchForWeeklySlotsPreviousWeek");
+            if (backWeekLink) 
+            {
+                backWeekLink.click();
+                await VratiSeUnazad();
+            }
+            else await FindAvailableSpots();
+            
+        }
+
         async function NextWeek()
         {
             await delay(1200);
@@ -52,5 +71,30 @@
             await FindAvailableSpots();
         }
 
+        function isMoreThanTwoMonthsFromToday(dateRangeString) {
+            let firstDate = dateRangeString.split("–")[0].trim();
+
+            // Remove the ordinal suffixes (e.g., "th", "rd", "nd", "st") from the day.
+            let day = firstDate.split(" ")[0].replace(/\D/g, "");
+
+            // Extract the month and year.
+            let monthName = firstDate.split(" ")[1];
+            let year = firstDate.split(" ")[2];
+
+            // Create a date object for today's date.
+            let today = new Date();
+
+            // Create a date object for the first date from the given range.
+            let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            let monthIndex = monthNames.indexOf(monthName);
+            let date = new Date(year, monthIndex, day);
+
+            // Calculate the difference in months.
+            let monthDifference = (date.getFullYear() - today.getFullYear()) * 12 + date.getMonth() - today.getMonth();
+
+            // Return true if the month difference is more than 2, false otherwise.
+            return monthDifference > 2 || (monthDifference === 2 && today.getDate() < date.getDate());
+        }
+        
     }
 })();
